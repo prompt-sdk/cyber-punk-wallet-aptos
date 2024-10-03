96d06770efa3ce52299deb8305fc7f4f41061294
nyansilence <47050063+nyansilence@users.noreply.github.com>
2024-10-03 22:09:07 +0700

    [FE] add UI chat popup

--- src/app/[locale]/(private)/dashboard/page.tsx
+++ src/app/[locale]/(private)/dashboard/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { PageBaseProps } from '@/common/interfaces';
-
 import DashboardRoot from '@/modules/dashboard/components/dashboard-root';
 
 type PageProps = PageBaseProps;
