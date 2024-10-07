ca0b46d9ebde609e83d3a160436b3814fd4be0e4
nyansilence <47050063+nyansilence@users.noreply.github.com>
2024-10-08 06:55:05 +0700

    update

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,16 @@ import { LocalePrefix, Pathnames } from 'next-intl/routing';
 
 export const localeDetection = false;
 export const defaultLocale = 'en-us';
-export const publicPages = ['/', '/login', '/chat', '/callback', '/chat/(.*)'];
+export const publicPages = [
+  '/',
+  '/login',
+  '/chat',
+  '/callback',
+  '/chat/(.*)',
+  '/create-agent',
+  '/create-widget',
+  '/create-tool'
+];
 export const locales = ['en-us', 'vi-vn'] as const;
 export const localePrefix = { mode: 'as-needed' } satisfies LocalePrefix<typeof locales>;
 export const pathnames = {
@@ -11,5 +20,8 @@ export const pathnames = {
   '/dashboard': '/dashboard',
   '/chat': '/chat',
   '/chat/[id]': '/chat/[id]',
-  '/callback': '/callback'
+  '/callback': '/callback',
+  '/create-agent': '/create-agent',
+  '/create-widget': '/create-widget',
+  '/create-tool': '/create-tool'
 } satisfies Pathnames<typeof locales>;
