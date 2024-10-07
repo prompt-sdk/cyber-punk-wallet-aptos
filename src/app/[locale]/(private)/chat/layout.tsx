3e41b388a5339d06637e8135988b9308834f97b5
nyansilence <47050063+nyansilence@users.noreply.github.com>
2024-10-08 06:36:27 +0700

    update UI chat

---
+++ src/app/[locale]/(private)/chat/layout.tsx
@@ -0,0 +1,41 @@
+import { Nunito, Orbitron } from 'next/font/google';
+import classNames from 'classnames';
+import { LayoutProps } from '@/common/interfaces';
+
+import MainFooter from '@/components/common/footers/main-footer';
+import MainHeader from '@/components/common/headers/main-header';
+import Body from '@/components/common/layout/body';
+import Head from '@/components/common/layout/head';
+import Html from '@/components/common/layout/html';
+import Root from '@/components/common/layout/root';
+
+import { ToastProvider } from '@/modules/toast/context/toast.context';
+
+const fontNunito = Nunito({
+  subsets: ['vietnamese'],
+  variable: '--font-nunito',
+  display: 'swap',
+  weight: ['300', '400', '500', '700']
+});
+
+const fontOrbitron = Orbitron({
+  subsets: ['latin'],
+  variable: '--font-orbitron',
+  display: 'swap',
+  weight: ['400', '500', '700', '800', '900']
+});
+
+export default async function PublicLayout({ children, params }: LayoutProps) {
+  return (
+    <Html locale={params.locale}>
+      <Head />
+      <Body className={classNames(fontNunito.variable, fontOrbitron.variable, fontOrbitron.className)}>
+        <Root>
+          <ToastProvider>
+            <div className="flex w-full grow flex-col overflow-hidden">{children}</div>
+          </ToastProvider>
+        </Root>
+      </Body>
+    </Html>
+  );
+}
