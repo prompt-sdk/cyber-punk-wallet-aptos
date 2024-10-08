bab5ce965e017c18b48fcc67cd9318ec82e33549
louisdevzz <vohuunhan1310@gmail.com>
2024-10-08 08:11:05 +0700

    fix: push github

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nextConfig = async () => {
       KV_REST_API_URL: process.env.KV_REST_API_URL,
       KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
       KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
-      APTOS_NETWORK: process.env.APTOS_NETWORK,
+      APTOS_NETWORK: process.env.APTOS_NETWORK
     },
     eslint: {
       ignoreDuringBuilds: false
