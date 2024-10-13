7908d3a221d575a96707d4dd7aedf7b8b2b2fd33
kurodenjiro <kurodenjiro1@gmail.com>
2024-10-13 17:13:28 +0700

    fix auth

---
+++ src/modules/auth/constants/auth.config.ts
@@ -0,0 +1,25 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+  pages: {
+    signIn: '/login'
+  },
+  callbacks: {
+    async jwt({ token, user }: { token: any; user: any }) {
+      if (user) {
+        token.id = user.id;
+        token.username = user.username;
+      }
+      return token;
+    },
+    async session({ session, token }: { session: any; token: any }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.username = token.username as string;
+      }
+      return session;
+    }
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: []
+} satisfies NextAuthConfig;
