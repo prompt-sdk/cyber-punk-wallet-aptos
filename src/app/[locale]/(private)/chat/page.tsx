ca4ce926530d47c17170473b2f32e020c22e9717
kurodenjiro <kurodenjiro1@gmail.com>
2024-10-16 01:17:51 +0700

    add widgetId

--- src/app/[locale]/(private)/chat/page.tsx
+++ src/app/[locale]/(private)/chat/page.tsx
@@ -7,6 +7,7 @@ import { Chat } from '@/modules/chat/components/chat-ui'
 import { AI } from '@/libs/chat/ai.actions'
 import { Session } from 'types/chat'
 import { nanoid } from '@/modules/chat/utils/utils';
+import { getToolIdByWidget, creatAgentWithTool } from '@/libs/db/store-mongodb';
 
 export const metadata = {
   title: 'Prompt Wallet'
@@ -16,19 +17,37 @@ export interface ChatPageProps {
   params: {
     agentId: string
     prompt: string
+    widgetId: string
   }
 }
 
 export default async function ChatPage({ params }: ChatPageProps) {
   const id = nanoid()
-  const session = (await auth()) as Session
+  const session: any = (await auth()) as Session
   const missingKeys = await getMissingKeys()
   // if have widget ID 
-  // get Tools fromt widget
-  // create agent with tool
-  //  
+  let agentId = null;
+  if (params.widgetId) {
+    // get Tools fromt widget
+    const tool_ids = getToolIdByWidget(params.widgetId);
+    // create agent with tool
+    const data: any = {
+      "name": "Smart Action",
+      "prompt": ` You are a Helpful developer.\n 
+            Analyze each query to determine if it requires plain text information or an action via a tool. Do not ever send tool call arguments with your chat. You must specifically call the tool with the information\n
+            For informational queries like "create label show balance of 0x123123123", respond with text, then balance of account you answered with using the 'getBlanace'. Always say something before or after tool usage.\n
+            Provide a response clearly and concisely. Always be polite, informative, and efficient.`,
+      "introMessage": "Im Aptos Bot",
+      "widget": [],
+      "user_id": session.user.username,
+      "tools": tool_ids,
+      "description": "This bot will excute transaction"
+    };
+    agentId = await creatAgentWithTool(data);
+  }
+
   return (
-    <AI initialAIState={{ chatId: id, messages: [], agentId: params.agentId }}>
+    <AI initialAIState={{ chatId: id, messages: [], agentId: agentId?.toString() || params.agentId }}>
       <Chat id={id} session={session} missingKeys={missingKeys} />
     </AI>
   )
--- src/libs/db/store-mongodb.tsx
+++ src/libs/db/store-mongodb.tsx
@@ -5,10 +5,7 @@ export const getTools = async (tool_ids: any[]) => {
     let db = clientPromsie.db('prompt');
     let col = await db.collection('tools');
     let oids: any[] = [];
-    tool_ids.forEach(function (item) {
-        oids.push(new ObjectId(item));
-    });
-    const data = await col.find({ _id: { "$in": oids } }).toArray();
+    const data = await col.find({ _id: { "$in": tool_ids.map((id: string) => new ObjectId(id)) } }).toArray();
     return data;
 };
 export const getToolIdByAgent = async (agentId: string) => {
@@ -20,3 +17,25 @@ export const getToolIdByAgent = async (agentId: string) => {
     const data = await col.find({ _id: new ObjectId(agentId) }).toArray();
     return data;
 };
+
+export const getToolIdByWidget = async (Widget: string) => {
+    let client = new MongoClient(process.env.MONGO_DB as string);
+    let clientPromsie = await client.connect();
+    let db = clientPromsie.db('prompt');
+    let col = await db.collection('agent');
+
+    const data = await col.find({ _id: new ObjectId(Widget) }).toArray();
+    return data;
+};
+
+export const creatAgentWithTool = async (data: any) => {
+
+    console.log('data', data);
+    let client = new MongoClient(process.env.MONGO_DB as string);
+    let clientPromsie = await client.connect();
+    let db = clientPromsie.db('prompt');
+    let col = await db.collection('agent');
+    const updata = await col.insertOne(data);
+    return updata.insertedId;
+};
+
