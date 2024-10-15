817c1f0a9f53377c1a42e85d072b249a18ff2a61
kurodenjiro <kurodenjiro1@gmail.com>
2024-10-15 13:03:27 +0700

    update

--- src/modules/auth/constants/auth.config.ts
+++ src/modules/auth/constants/auth.config.ts
@@ -1,28 +1,5 @@
 import type { NextAuthConfig } from 'next-auth';
 import NextAuth from 'next-auth';
+import { authOptions } from '@/modules/auth/constants/auth.constant';
 
-export const authConfig = {
-  pages: {
-    signIn: '/login'
-  },
-  callbacks: {
-    async jwt({ token, user }: { token: any; user: any }) {
-      if (user) {
-        token.id = user.id;
-        token.username = user.username;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: any; token: any }) {
-      if (session.user) {
-        session.user.id = token.id as string;
-        session.user.username = token.username as string;
-      }
-      return session;
-    }
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: []
-} satisfies NextAuthConfig;
-
-export const { auth, signIn, signOut, handlers } = NextAuth(authConfig);
+export const { auth, signIn, signOut, handlers } = NextAuth(authOptions);
