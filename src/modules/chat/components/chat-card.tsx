e63c93bb63e00791445d58c6e00ae3c64a8b9be6
kurodenjiro <kurodenjiro1@gmail.com>
2024-10-17 16:37:08 +0700

    add errorboundy to chat

--- src/modules/chat/components/chat-card.tsx
+++ src/modules/chat/components/chat-card.tsx
@@ -17,7 +17,7 @@ import UserIcon from '@/assets/svgs/user-icon.svg'
 // Different types of message bubbles.
 import { useSession } from 'next-auth/react';
 import { useWallet } from '@aptos-labs/wallet-adapter-react';
-
+import { ErrorBoundary } from 'react-error-boundary';
 
 export function UserMessage({ children }: { children: React.ReactNode }) {
     const { data: session }: any = useSession();
@@ -66,7 +66,49 @@ export function BotMessage({
             <div className="grow">
                 {/* get chat id */}
                 <ChatMessageItem creator={'Smart Action'} isUser={false} >
-                    {text}
+                    <ErrorBoundary fallback={<div className='whitespace-pre-wrap'>...</div>}>
+                        <MemoizedReactMarkdown
+                            className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
+                            remarkPlugins={[remarkGfm, remarkMath]}
+                            components={{
+                                p({ children }) {
+                                    return <p className="mb-2 last:mb-0">{children}</p>
+                                },
+                                code({ node, inline, className, children, ...props }) {
+                                    if (children.length) {
+                                        if (children[0] == '▍') {
+                                            return (
+                                                <span className="mt-1 animate-pulse cursor-default">▍</span>
+                                            )
+                                        }
+
+                                        children[0] = (children[0] as string).replace('`▍`', '▍')
+                                    }
+
+                                    const match = /language-(\w+)/.exec(className || '')
+
+                                    if (inline) {
+                                        return (
+                                            <code className={className} {...props}>
+                                                {children}
+                                            </code>
+                                        )
+                                    }
+
+                                    return (
+                                        <CodeBlock
+                                            key={Math.random()}
+                                            language={(match && match[1]) || ''}
+                                            value={String(children).replace(/\n$/, '')}
+                                            {...props}
+                                        />
+                                    )
+                                }
+                            }}
+                        >
+                            {text}
+                        </MemoizedReactMarkdown>
+                    </ErrorBoundary>
                 </ChatMessageItem>
 
             </div>
