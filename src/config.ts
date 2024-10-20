d9afba1257cdcc8e311e4a300039e0493dbcd5ea
kurodenjiro <kurodenjiro1@gmail.com>
2024-10-20 20:00:24 +0700

    remove locale

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,3 @@
-import { LocalePrefix, Pathnames } from 'next-intl/routing';
-
-export const localeDetection = false;
-export const defaultLocale = 'en-us';
 export const publicPages = [
   '/',
   '/login',
@@ -12,8 +8,6 @@ export const publicPages = [
   '/create-widget',
   '/create-tool'
 ];
-export const locales = ['en-us', 'vi-vn'] as const;
-export const localePrefix = { mode: 'as-needed' } satisfies LocalePrefix<typeof locales>;
 export const pathnames = {
   '/': '/',
   '/login': '/login',
@@ -24,4 +18,4 @@ export const pathnames = {
   '/create-agent': '/create-agent',
   '/create-widget': '/create-widget',
   '/create-tool': '/create-tool'
-} satisfies Pathnames<typeof locales>;
+};
