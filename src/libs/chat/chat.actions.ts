c244ebf9705f4ba3d165e343fcae3ccc3f55cb46
kurodenjiro <kurodenjiro1@gmail.com>
2024-10-21 09:01:14 +0700

    fix remove chat , share chat

--- src/libs/chat/chat.actions.ts
+++ src/libs/chat/chat.actions.ts
@@ -67,29 +67,29 @@ export async function removeChat({ id, path }: { id: string; path: string }) {
   // Convert uid to string for consistent comparison with session.user.id
   const uid = String(await kv.hget(`chat:${id}`, 'userId'))
 
-  if (uid !== session?.user?.username) {
+  if (uid !== session?.user?.id) {
     return {
       error: 'Unauthorized'
     }
   }
 
   await kv.del(`chat:${id}`)
-  await kv.zrem(`user:chat:${session.user.username}`, `chat:${id}`)
+  await kv.zrem(`user:chat:${session.user.id}`, `chat:${id}`)
 
-  revalidatePath('/')
+  //revalidatePath('/')
   return revalidatePath(path)
 }
 
 export async function clearChats() {
   const session : any= await auth()
 
-  if (!session?.user?.username) {
+  if (!session?.user?.id) {
     return {
       error: 'Unauthorized'
     }
   }
 
-  const chats: string[] = await kv.zrange(`user:chat:${session.user.username}`, 0, -1)
+  const chats: string[] = await kv.zrange(`user:chat:${session.user.id}`, 0, -1)
   if (!chats.length) {
     return redirect('/')
   }
@@ -97,7 +97,7 @@ export async function clearChats() {
 
   for (const chat of chats) {
     pipeline.del(chat)
-    pipeline.zrem(`user:chat:${session.user.username}`, chat)
+    pipeline.zrem(`user:chat:${session.user.id}`, chat)
   }
 
   await pipeline.exec()
@@ -127,7 +127,7 @@ export async function shareChat(id: string) {
 
   const chat = await kv.hgetall<Chat>(`chat:${id}`)
 
-  if (!chat || chat.userId !== session.user.username) {
+  if (!chat || chat.userId !== session.user.id) {
     return {
       error: 'Something went wrong'
     }
